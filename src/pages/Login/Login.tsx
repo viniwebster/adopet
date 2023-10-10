1a121029f28b3798027c2c9b3a4235af0139e587
vinicius webster <viniciuswebster99@gmail.com>
2023-10-09 21:12:57 -0300

    fix: navigate

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,7 +3,10 @@ import Button from "components/Button/Button";
 import Input from "components/Input/Input";
 import { IUser } from "interfaces/IUser";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useSetAuthUser from "state/hooks/useSetAuthUser";
 import styled from "styled-components";
+import Logo from "assets/Logo-Azul.svg";
 
 interface PropsStyledForm {
   $width?: string;
@@ -14,6 +17,10 @@ export const StyledForm = styled.form<PropsStyledForm>`
   flex-direction: column;
   align-items: center;
   width: ${({ $width }) => ($width ? $width : "100%")};
+
+  img{
+    margin-bottom: 50px;
+  }
 `;
 
 const Login = () => {
@@ -22,6 +29,8 @@ const Login = () => {
 
   const [message, setMessage] = useState<string>("");
   const findUser = sessionStorage.getItem(`${email}`);
+  const logged = useSetAuthUser();
+  const navigate = useNavigate();
 
   const onSubmitLogin = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -31,7 +40,8 @@ const Login = () => {
       user = JSON.parse(findUser);
       if (user.email === email && user.password === password) {
         setMessage(`Bem vindo ${user.name} :)`);
-        window.location.pathname = "/pets";
+        logged(true);
+        navigate('/pets')
         setTimeout(() => {
           setMessage("");
         }, 5000);
@@ -47,6 +57,8 @@ const Login = () => {
   return (
     <StyledForm onSubmit={onSubmitLogin}>
       <StyledBodyLight />
+      <img src={Logo} alt="Logo adopet" />
+
       <StyledParagraph color="#3772FF">
         Já tem conta? Faça seu login
       </StyledParagraph>
